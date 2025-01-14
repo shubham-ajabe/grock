826243a0dbc1982cd04d16b7adcbfb764fcaffe7
Shubham Ajabe <66984159+shubham-ajabe@users.noreply.github.com>
2025-01-14 11:56:54 +0530

    Update home.py

--- home.py
+++ home.py
@@ -21,8 +21,27 @@ def webhook_handler():
     else:
         return jsonify({"status": "error", "message": "Invalid data format!"}), 400
 
-
+# Add routes for additional pages
 @home.route('/about_us')  # About Us route
 def about_us():
     return render_template('about_us.html')
 
+@home.route('/our_services')  # Our Services route
+def our_services():
+    return render_template('our_services.html')
+
+@home.route('/business_sector')  # Business Sector route
+def business_sector():
+    return render_template('business_sector.html')
+
+@home.route('/blog')  # Blog route
+def blog():
+    return render_template('blog.html')
+
+@home.route('/quick_links')  # Quick Links route
+def quick_links():
+    return render_template('quick_links.html')
+
+@home.route('/our_vision')  # Our Vision route
+def our_vision():
+    return render_template('our_vision.html')
